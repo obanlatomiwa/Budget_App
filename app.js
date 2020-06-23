9544c3be07d538d49e5dfedc34b66b737210b59b
Tomiwa Obanla <obanlatomiwa@gmail.com>
2020-06-23 05:56:50 +0100

    add app controllers

---
+++ app.js
@@ -0,0 +1,19 @@
+// Budget App
+
+// Budget Controller
+let budgetController = (function(){
+
+})();
+
+
+// UI Controller
+let UIController = (function(){
+
+
+})();
+
+
+// App Controller
+let appController = (function(){
+
+})();
