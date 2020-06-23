8add3676d9e3226adf5b6e9e88101489c9b67db4
Tomiwa Obanla <obanlatomiwa@gmail.com>
2020-06-23 06:26:08 +0100

    more changes

--- app.js
+++ app.js
@@ -16,4 +16,24 @@ let UIController = (function(){
 // App Controller
 let appController = (function(){
 
+    // add items
+    let addCtrlItem = function(){
+        // get data from the input 
+
+        // send to the budget controller
+
+        // send to the UI controller
+
+        // calculate the new budget
+
+        // display new budget to the UI 
+    }
+    document.querySelector('.add__btn').addEventListener('click', addCtrlItem);
+    document.addEventListener('keypress', function(event){
+        // ensure the Keyboard Enter key, adds items
+        if (event.keyCode === 13 || event.which === 13){
+            addCtrlItem();
+        }
+    });
+
 })();
